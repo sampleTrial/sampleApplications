45c542acca8ecabc1bd863bef2f5e8f98dbe13a1
dixyushi <72491437+dixyushi@users.noreply.github.com>
2021-09-01 08:15:50 +0530

    testing entity

--- springlogging/src/test/java/com/example/logging/springlogging/SpringloggingApplicationTests.java
+++ springlogging/src/test/java/com/example/logging/springlogging/SpringloggingApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.logging.springlogging;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class SpringloggingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+
+  }
+
+  @LocalServerPort
+  private int port;
+  
+  @Autowired
+  private SampleLogging sampleLogging;
+
+  @Test
+  public void testGreeting() throws Exception {
+
+    ResponseEntity<String> entity = this.sampleLogging.getForEntity(
+        "http://localhost:" + this.port + "/eureka/apps", String.class).
+    then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
 
 }
